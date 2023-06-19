545e5209965fccd3ad12652c8e51247f647ef580
jpnsantoss <jpnsantos14@gmail.com>
2023-06-19 01:04:15 +0100

    Basic Setup.

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Open_Sans } from "next/font/google";
+
+export const metadata = {
+  title: "Norasil",
+  description:
+    "Fundada em Maio de 1983, a Norasil – Sociedade de Construção Civil, S.A., (com Sede e Estaleiro Central em Matosinhos) tem como objecto principal a execução de empreitadas de obras públicas e de obras particulares.",
+};
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        openSans.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
