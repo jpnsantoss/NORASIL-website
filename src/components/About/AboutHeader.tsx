76785dba8479a6a67085b250bab696171b066e89
jpnsantoss <jpnsantos14@gmail.com>
2023-11-08 08:22:38 +0000

    link from company header fixed

--- src/components/About/AboutHeader.tsx
+++ src/components/About/AboutHeader.tsx
@@ -1,7 +1,8 @@
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import { FC } from "react";
-import { Button } from "../ui/Button";
+import { Button, buttonVariants } from "../ui/Button";
 
 interface AboutHeaderProps {}
 
@@ -12,7 +13,7 @@ const AboutHeader: FC<AboutHeaderProps> = ({}) => {
     <div className="w-full px-4 lg:px-48 lg:py-16">
       <h2 className="text-2xl font-semibold mb-2">De obra em obra,</h2>
       <div className="grid lg:grid-cols-2 gap-16">
-        <div className="flex flex-col justify-between">
+        <div className="space-y-8">
           <h1
             className={cn(
               inter.className,
@@ -21,12 +22,14 @@ const AboutHeader: FC<AboutHeaderProps> = ({}) => {
           >
             Vamos construindo o futuro, seguramente.
           </h1>
-          <Button
-            size={"lg"}
-            className="text-xl font-bold py-6 my-4 px-10 w-fit rounded-lg shadow-md shadow-secondary"
-          >
-            Ver Obras
-          </Button>
+          <div>
+            <Link
+              href={"/portfolio"}
+              className={cn(buttonVariants({ size: "lg" }), "my-4")}
+            >
+              Ver obras
+            </Link>
+          </div>
         </div>
         <div className="space-y-4 texl-xl lg:text-2xl mt-4">
           <h2 className="font-bold">
